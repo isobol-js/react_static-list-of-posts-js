a7dc96ec90504e41e3d746ed9bde36762fd3c97c
iulii sobol <soboliulii@gmail.com>
2025-08-03 11:08:10 +0300

    solution

--- src/index.jsx
+++ src/index.jsx
@@ -3,3 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 createRoot(document.getElementById('root')).render(<App />);
+
+
+function getNumbers(grades) {
+
+  const sum = grades.filter(grade => grade > 90)
+                    .map(grade => grade + 5)
+                    .reduce((acc, grade) => acc + grade, 0);
+
+    const total = sum / grades.length;
+    return total;
+}
